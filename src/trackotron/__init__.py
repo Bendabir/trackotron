40752e2cc66aecdb186aa677d117e90b78e18ceb
Benjamin Riviere <briviere01@gmail.com>
2024-09-07 14:42:10 +0200

    Add some base types.

--- src/trackotron/__init__.py
+++ src/trackotron/__init__.py
@@ -1,1 +1,7 @@
 """Copyright (c) 2024 Bendabir."""
+
+from __future__ import annotations
+
+from .types_ import ObservationType
+
+__all__ = ["ObservationType"]
---
+++ src/trackotron/types_.py
@@ -0,0 +1,13 @@
+"""Copyright (c) 2024 Bendabir."""
+
+from __future__ import annotations
+
+from typing import Literal, TypeVar
+
+from typing_extensions import ParamSpec
+
+P = ParamSpec("P")
+R_co = TypeVar("R_co", covariant=True)
+T_contra = TypeVar("T_contra", contravariant=True)
+
+ObservationType = Literal["span", "generation", "event"]
