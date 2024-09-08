bb344129cdd4127407e1996ab8be301c82509ffa
Benjamin Riviere <briviere01@gmail.com>
2024-09-08 09:19:26 +0200

    Make version available from root module.

--- src/trackotron/__init__.py
+++ src/trackotron/__init__.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from ._version import __version__
 from .contexts import EventContext, GenerationContext, ObservationContext, SpanContext
 from .observer import Observer
 from .proxies import (
@@ -34,4 +35,5 @@ __all__ = [
     "SpanProxy",
     "SpanProxyAlias",
     "SpanUpdate",
+    "__version__",
 ]
